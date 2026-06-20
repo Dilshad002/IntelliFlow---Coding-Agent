6916f26ff55964561706a2b9eab4c4564ce88d3c
Dilshad002 <dilshadd2034@gmail.com>
2026-06-20 10:27:36 +0000

    updated api.py with FastAPI endpoint /run

---
+++ api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from agent import app as agent_app
+
+app = FastAPI()
+
+class RunRequest(BaseModel):
+    user_prompt: str
+
+@app.post('/run')
+def run(request: RunRequest):
+    result = agent_app.invoke({
+        "user_prompt": request.user_prompt,
+        "code": "",
+        "output": "",
+        "error": "",
+        "attempts": 0,
+        "success": False
+    })
+    out = {"code": result["code"], 
+            "output": result["output"], 
+            "error": result["error"], 
+            "attempts": result["attempts"], 
+            "success": result["success"]
+            }
+    return out
